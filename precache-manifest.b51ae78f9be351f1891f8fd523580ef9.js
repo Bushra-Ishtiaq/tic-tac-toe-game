53a01cb2ff8652e339dfe595eaba4ef94a3f2e0f
Bushra-Ishtiaq <bushraishtiaq50@gmail.com>
2020-11-22 07:24:01 -0800

    Updates

---
+++ precache-manifest.b51ae78f9be351f1891f8fd523580ef9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8571d5efa5e1e0161bcfc486e5c11c69",
+    "url": "/tic-tac-toe-game/index.html"
+  },
+  {
+    "revision": "da4f6669566af04228ce",
+    "url": "/tic-tac-toe-game/static/js/2.30b555f6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/tic-tac-toe-game/static/js/2.30b555f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54533b35184098cc5cf3",
+    "url": "/tic-tac-toe-game/static/js/main.d319e703.chunk.js"
+  },
+  {
+    "revision": "27ef5eb149ff98f71fbd",
+    "url": "/tic-tac-toe-game/static/js/runtime-main.4156cc54.js"
+  }
+]);
